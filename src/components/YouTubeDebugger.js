303ca396c88132a1680af87554ac9045891f5e33
Mic Fox <michael.c.fox.ii@gmail.com>
2019-10-23 13:20:14 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
+import { realpathSync } from "fs";
+
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+    constructor(){
+    super()
+
+    this.state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8, 
+            video: {
+                resolution: '1080p'
+            }
+        }
+
+    }
+}
+
+    handleBitrate = () => {
+        let setBitrate = {
+            ...this.state.settings,
+            bitrate: 12
+        }
+        this.setState({
+            settings: setBitrate
+        })
+    }
+    handleResolution = () => {
+        let setResolution = {
+            ...this.state.settings,
+            video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+            }
+        }
+            this.setState({
+                settings: setResolution
+            })
+    }
+    
+
+    render() {
+        return (
+            <div>
+            <button className='resolution' onClick={this.handleResolution}> Resolution </button>
+            <button className='bitrate' onClick={this.handleBitrate}> BitRate </button>
+            </div>
+        )
+    }
+}
